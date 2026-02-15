049ab6f53dc8f917e13d3e06012e94343f7f6ea8
AshfakAhmath <ashfakahmath57@gmail.com>
2026-02-15 14:44:06 +0530

    Add string slicing practice with email parsing

---
+++ javaScript Practice/string-methods-practice.js/demo.js
@@ -0,0 +1,12 @@
+// let fullName = "Ashfak Ahmath";
+// let firstName = fullName.slice(0, fullName.indexOf(" "));
+// let lastName = fullName.slice(fullName.indexOf(" ") + 1);
+// console.log(firstName);
+// console.log(lastName);
+
+const email = "ashfak1@gmail.com";
+let userName = email.slice(0, email.indexOf("@"));
+let extension = email.slice(email.indexOf("@")+1);
+
+console.log(userName);
+console.log(extension);
