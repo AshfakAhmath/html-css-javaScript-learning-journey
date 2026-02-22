dcfc7650452af55d513193c3d21f3346920622aa
AshfakAhmath <ashfakahmath57@gmail.com>
2026-02-22 17:46:54 +0530

    Task Tracker UI

---
+++ 04-task-tracker/index.js
@@ -0,0 +1,7 @@
+const getTask = () => {
+    const taskInput = document.getElementById("taskInput").value;
+    const task1 = document.getElementById("task1");
+
+    task1.textContent = taskInput;
+    
+}
